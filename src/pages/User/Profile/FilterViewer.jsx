7cf382596a7f58920a6b7e3cff20d121293a9a6f
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-12-21 22:42:24 -0800

    revise filter

--- src/pages/User/Profile/FilterViewer.jsx
+++ src/pages/User/Profile/FilterViewer.jsx
@@ -1,10 +1,20 @@
-import { Dialog } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, Typography } from "@mui/material";
 import React from "react";
+import MyIconButton from "../../../components/Customized/MyIconButton";
+import { Close } from "@mui/icons-material";
 
 export default function FilterViewer({ open, onClose, setViewer }) {
   return (
-    <Dialog open={open} onClose={onClose}>
-      FilterViewer
+    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+      <DialogTitle
+        sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
+      >
+        <Typography sx={{ mr:"auto" }}>Select audience </Typography>
+        <MyIconButton>
+          <Close />
+        </MyIconButton>
+      </DialogTitle>
+      <DialogContent></DialogContent>
     </Dialog>
   );
 }

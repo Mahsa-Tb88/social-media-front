403d274a7ed98ca5b98d6b869805cea4220b84bc
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-05-07 20:28:50 -0700

    add emoji to leave comment

--- src/pages/User/Profile/component/shared/post/comment/InputComment.jsx
+++ src/pages/User/Profile/component/shared/post/comment/InputComment.jsx
@@ -7,10 +7,12 @@ import { useSelector } from "react-redux";
 import MyIconButton from "../../../../../../../components/Customized/MyIconButton";
 import MoodIcon from "@mui/icons-material/Mood";
 import { green } from "@mui/material/colors";
+import EmojiPicker from "emoji-picker-react";
 
 export default function InputComment({ postId, replyTo, setReply }) {
   const theme = useSelector((state) => state.app.theme);
   const [text, setText] = useState("");
+  const [showEmoji, setShowEmoji] = useState(false);
   const userLogin = useSelector((state) => state.user.profile);
   const mutation = useLeaveComment();
   const queryClient = useQueryClient();
@@ -35,46 +37,74 @@ export default function InputComment({ postId, replyTo, setReply }) {
     });
   }
 
+  function handleEmoji(emojiData) {
+    const emoji = emojiData.emoji;
+    // Insert emoji at the cursor position
+    const newText = text + emoji;
+    setText(newText);
+  }
+  console.log(" Text", text);
+
   return (
-    <Stack
-      sx={{
-        mt: 3,
-        px: 1,
-        gap: 2,
-        flexDirection: "row",
-        alignItems: "center",
-        borderRadius: "6px",
-        border: "1px solid",
-        borderColor: theme === "dark" ? "grey.800" : "grey.200",
-        bgcolor: theme === "dark" ? "grey.800" : "grey.200",
-        "&:focus": {
-          outline: "none",
-          borderColor: theme === "dark" ? "grey.200" : "grey.800",
-        },
-      }}
-    >
-      <TextField
-        placeholder="Write your comment"
-        multiline
+    <Box sx={{ position: "relative", width: "100%" }}>
+      <Stack
         sx={{
-          width: "100%",
-          fontSize: 15,
+          mt: 3,
           px: 1,
-          py: 1,
+          gap: 2,
+          flexDirection: "row",
+          alignItems: "center",
+          borderRadius: "6px",
+          border: "1px solid",
+          borderColor: theme === "dark" ? "grey.800" : "grey.200",
+          bgcolor: theme === "dark" ? "grey.800" : "grey.200",
+          "&:focus": {
+            outline: "none",
+            borderColor: theme === "dark" ? "grey.200" : "grey.800",
+          },
         }}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        error={text.length >= 1100}
-        inputProps={{ maxLength: 1100 }}
-      />
-      <MyIconButton tooltip="post" onClick={() => setShowEmoji(!showEmoji)}>
-        <MoodIcon sx={{ color: green[500] }} />
-      </MyIconButton>
-      <Box sx={{ cursor: "pointer" }} onClick={sendText}>
-        <SendIcon
-          sx={text ? { color: "#1976d2", "&:hover": { color: "#1769aa" } } : ""}
+      >
+        <TextField
+          placeholder="Write your comment"
+          multiline
+          sx={{
+            width: "100%",
+            fontSize: 15,
+            px: 1,
+            py: 1,
+          }}
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+            setShowEmoji(false);
+          }}
+          error={text.length >= 1100}
+          inputProps={{ maxLength: 1100 }}
         />
-      </Box>
-    </Stack>
+        <MyIconButton tooltip="post" onClick={() => setShowEmoji(!showEmoji)}>
+          <MoodIcon sx={{ color: green[500] }} />
+        </MyIconButton>
+
+        <Box sx={{ cursor: "pointer" }} onClick={sendText}>
+          <SendIcon
+            sx={
+              text ? { color: "#1976d2", "&:hover": { color: "#1769aa" } } : ""
+            }
+          />
+        </Box>
+      </Stack>
+      {showEmoji && (
+        <Stack
+          sx={{
+            position: "absolute",
+            top: "100%", // or adjust like `top: 60px` depending on design
+            right: 0,
+            zIndex: 10,
+          }}
+        >
+          <EmojiPicker onEmojiClick={handleEmoji} />
+        </Stack>
+      )}
+    </Box>
   );
 }

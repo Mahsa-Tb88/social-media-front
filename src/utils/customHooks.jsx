56c874607b7c84f0504ed8e42e9328186b6663bb
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-06-02 15:59:49 -0700

    revise

--- src/utils/customHooks.jsx
+++ src/utils/customHooks.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 export function useRedurectIfIsLoggedIn() {
   const user = useSelector((state) => state.user);
-  console.log("user is", user);
   const navigate = useNavigate();
   useEffect(() => {
     navigate(user.isLoggedIn ? `/profile/${user.profile.id}` : "/login");
